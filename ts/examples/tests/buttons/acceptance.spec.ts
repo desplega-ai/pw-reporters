0893480d6d7e3aaa3d1eb852b1ee92baf42dfd12
2pac <tarasyaremabcn@gmail.com>
2025-12-10 01:10:14 +0100

    chore: acceptance changes

--- ts/examples/tests/buttons/acceptance.spec.ts
+++ ts/examples/tests/buttons/acceptance.spec.ts
@@ -17,7 +17,10 @@ test.describe(
 
     test("should be able to click pill", async ({ page }) => {
       await p.page.goto("/buttons");
+      await p.page.waitForTimeout(5000);
       await p.page.getByRole("button", { name: "Pill button" }).click();
+      await p.page.waitForTimeout(5000);
+      console.log("Clicked Pill button");
       await expect(page.getByText("Selected")).toBeVisible();
     });
   },
